3eb491136edf09a2f39ce7d82d08b57debb0de12
Liana <liana.ivanova@yahoo.com>
2022-04-17 00:28:59 -0700

    Add warning msg to custom exception

--- src/CommandNotFoundException.java
+++ src/CommandNotFoundException.java
@@ -2,4 +2,8 @@ public class CommandNotFoundException extends Exception {
     public CommandNotFoundException(String message) {
         super(message);
     }
+
+    public void printWarningMsg() {
+        System.out.println(getMessage() + "\nPlease use one of the known commands!");
+    }
 }
